b8f643a54455cb0b3229d0afc719f914ee485729
Syahri Rhamadhan <2001020038@student.umrah.ac.id>
2023-07-09 23:21:39 +0700

    fix cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+// import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 import FileUpload from "express-fileupload";
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
  
 // app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
-app.use(cors())
+// app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
