640e7e0b168bb16a3dbc6dcf33f25cc2a09382c6
Syahri Rhamadhan <2001020038@student.umrah.ac.id>
2023-07-09 22:15:56 +0700

    tes deploy

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 // Berisi konfigurasi untuk setting database pada Railway
 import { Sequelize } from "sequelize";
-const db = new Sequelize('railway', 'root', 'TDFb9uW2JVDXCiahCdqy', {
-    host: "containers-us-west-164.railway.app",
-    port: "7497",
-    dialect: "mysql"
+const db = new Sequelize("railway", "root", "VpPGmZXBwTFO3J3CB28x", {
+  host: "containers-us-west-84.railway.app",
+  port: "6709",
+  dialect: "mysql",
 });
 export default db;
