2f0158c6f3b292e5c3cd41cec75cc47d192991e9
Syahri Rhamadhan <2001020038@student.umrah.ac.id>
2023-07-09 22:34:44 +0700

    fix npm start

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.use(router);
 (async () => {
     await db.sync();
 })();
-app.listen(process.env.PORT, ()=> console.log(`server is running`))
+app.listen(process.env.PORT, ()=> console.log(`server is running http://localhost:5000`))
